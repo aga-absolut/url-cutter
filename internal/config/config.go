efd30bac8aeaea428fa8b7c391ed1894b72b1c59
aga-im <agafendii@gmail.com>
2025-12-21 17:02:09 +0300

    last changes

--- internal/config/config.go
+++ internal/config/config.go
@@ -11,7 +11,7 @@ func NewConfig() *Config {
 	cfg := &Config{}
 
 	flag.StringVar(&cfg.Host, "a", ":8080", "server host:port to listen on")
-	flag.StringVar(&cfg.ServerAddress, "b", "", "base URL for shortened links")
+	flag.StringVar(&cfg.ServerAddress, "b", "http://localhost:8080/", "base URL for shortened links")
 
 	flag.Parse()
 	return  cfg
