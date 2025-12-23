d8d124a536425dae016b3151afdfd108aebe0e21
aga-im <agafendii@gmail.com>
2025-12-23 13:27:33 +0300

    solve the problem

--- internal/config/config.go
+++ internal/config/config.go
@@ -12,18 +12,18 @@ type Config struct {
 
 func NewConfig() *Config {
 	cfg := &Config{}
-	
+
 	flag.StringVar(&cfg.Host, "a", ":8080", "server host:port to listen on")
 	flag.StringVar(&cfg.ServerAddress, "b", "http://localhost:8080", "base URL for shortened links")
 
 	flag.Parse()
 
-	if envHost := os.Getenv("HOST"); envHost != ""{
+	if envHost := os.Getenv("SERVER_ADDRESS"); envHost != "" {
 		cfg.Host = envHost
 	}
-	if envAddress := os.Getenv("SERVER_ADDRESS"); envAddress != ""{
-		cfg.ServerAddress = envAddress
+	if envAddress := os.Getenv("BASE_URL"); envAddress != "" {
+		cfg.ServerAddress = "http://" + envAddress
 	}
-	
+
 	return cfg
 }
