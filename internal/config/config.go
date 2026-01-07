94fcce62ee6d712a781a8280b8041fc4596bec8f
aga-im <agafendii@gmail.com>
2026-01-07 14:43:43 +0300

    tryin version 2

--- internal/config/config.go
+++ internal/config/config.go
@@ -19,10 +19,7 @@ func NewConfig() *Config {
 	if err := env.Parse(cfg); err != nil {
 		panic(err)
 	}
-
-	if cfg.FilePath != "" && cfg.Host != "" && cfg.ServerAddress != "" {
-		return cfg
-	}
+	
 	flag.StringVar(&cfg.Host, "a", ":8080", "server host:port to listen on")
 	flag.StringVar(&cfg.ServerAddress, "b", "http://localhost:8080/", "base URL for shortened links")
 	flag.StringVar(&cfg.FilePath, "f", "storage.txt", "storage filename")
