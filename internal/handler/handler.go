611fdf08b61561eb4e3585ed01037087018ac850
aga-im <agafendii@gmail.com>
2026-01-18 14:31:31 +0300

    final version

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -109,8 +109,7 @@ func (h *Handler) PostBatchHandler(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 		h.memory.Set(shortKey, v.OriginalURL)
-		h.file.Set(shortKey, v.OriginalURL)
-		h.pgxDB.Set(shortKey, v.OriginalURL)
+
 		responseItem.ShortURL = h.config.Host + "/" + shortKey
 		responseItem.CorrelationID = v.CorrelationID
 		response = append(response, responseItem)
