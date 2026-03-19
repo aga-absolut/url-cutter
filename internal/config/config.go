33f3a6743409dec8c860b4998ec967ec80552849
aga-im <agafendii@gmail.com>
2026-03-19 11:15:00 +0300

    1

--- internal/config/config.go
+++ internal/config/config.go
@@ -29,8 +29,8 @@ func NewConfig() *Config {
 	cfg := &Config{}
 	flag.StringVar(&cfg.ServerAddress, "a", "localhost:8080", "server host:port")
 	flag.StringVar(&cfg.Host, "b", "http://localhost:8080", "base URL")
-	flag.StringVar(&cfg.FilePath, "f", "", "storage filename")                                                               // storage.txt
-	flag.StringVar(&cfg.DBDSN, "d", "postgres://postgres:absolute_1@localhost:5432/mydb", "name for check connect database") // psql -U postgres -d mydb -W
+	flag.StringVar(&cfg.FilePath, "f", "", "storage filename")             // storage.txt
+	flag.StringVar(&cfg.DBDSN, "d", "", "name for check connect database") // postgres://postgres:absolute_1@localhost:5432/mydb
 	flag.Parse()
 
 	if err := env.Parse(cfg); err != nil {
