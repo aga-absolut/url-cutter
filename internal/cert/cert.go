7a790449cc7e522257f0a88742e48615c423fd89
aga-im <agafendii@gmail.com>
2026-04-03 17:37:59 +0300

    iter23

--- internal/cert/cert.go
+++ internal/cert/cert.go
@@ -13,7 +13,7 @@ import (
 	"time"
 )
 
-// Функция создания сертификата
+// GenerateCertificate создает сертификат
 func GenerateCertificate(certFile, keyFile string) error {
 	// создаём шаблон сертификата
 	cert := &x509.Certificate{
