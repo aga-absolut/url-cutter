f4a33541de0570e8a41a0412c4275ea3d365f443
aga-im <agafendii@gmail.com>
2026-04-12 13:39:18 +0300

    fix errors bug

--- internal/errs/errs.go
+++ internal/errs/errs.go
@@ -3,13 +3,13 @@ package errs
 import "errors"
 
 var (
-	ErrInGettingUserID      = errors.New("error in getting id")
-	ErrInGettingURLs        = errors.New("error in getting urls")
-	ErrEmptyBody            = errors.New("error empty body")
-	ErrEmptyBatch           = errors.New("error empty batch")
+	ErrInGettingUserID      = errors.New("getting id")
+	ErrInGettingURLs        = errors.New("getting urls")
+	ErrEmptyBody            = errors.New("empty body")
+	ErrEmptyBatch           = errors.New("empty batch")
 	ErrURLAlreadyExists     = errors.New("url already exists")
-	ErrTrustedSubnetIsEmpty = errors.New("error empty trustedSubnet")
-	ErrInGettingUrlsCount   = errors.New("error In Getting Urls Count")
-	ErrNilIP                = errors.New("error nil ip")
-	ErrIPNotAllowed         = errors.New("error ip not allowed")
+	ErrTrustedSubnetIsEmpty = errors.New("empty trustedSubnet")
+	ErrInGettingUrlsCount   = errors.New("пetting Urls Count")
+	ErrNilIP                = errors.New("nil ip")
+	ErrIPNotAllowed         = errors.New("ip not allowed")
 )
